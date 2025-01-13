40264bb6a1512b6e32cf10b2b54bc7e9cd51a860
Neo Kai Yuan <3345016+neokaiyuan@users.noreply.github.com>
2025-01-13 21:40:23 +0800

    Layout navbar with logo in center and sell button on right

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
 import ListingPreviewList from "./ListingPreviewList";
 
 const Home = () => {
   return (
     <div>
-      <Link to="/listings/new">Sell</Link>
-      <br />
-      <br />
       <ListingPreviewList />
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,20 @@
 import React from "react";
 import logo from "../logo.png"; // Adjust the path if necessary
 import "./NavBar.css"; // Update the CSS file import
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className="nav-bar">
-      <img src={logo} alt="Logo" className="logo" />
+      <div className="left-content">
+        {/* Any content you want on the left side */}
+      </div>
+      <Link to="/">
+        <img src={logo} alt="Logo" className="logo" />
+      </Link>
+      <Link to="/listings/new" className="sell-button">
+        Sell
+      </Link>
     </div>
   );
 };
