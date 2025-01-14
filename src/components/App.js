f9d9c430a6ee459d12ced061ddfae4eb13d1032a
Neo Kai Yuan <3345016+neokaiyuan@users.noreply.github.com>
2025-01-14 11:08:09 +0800

    Align items in navbar and body

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import "./App.css";
 import NavBar from "./NavBar";
 
-const App = () => (
-  <div className="App">
-    <NavBar />
-    <Outlet />
-  </div>
-);
+const App = () => {
+  const { user, isAuthenticated } = useAuth0();
+
+  return (
+    <div className="App">
+      <NavBar userName={isAuthenticated ? user.name : null} />
+      <div className="main-content">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,20 +3,39 @@ import logo from "../logo.png"; // Adjust the path if necessary
 import "./NavBar.css"; // Update the CSS file import
 import { Link } from "react-router-dom";
 
-const NavBar = () => {
+function NavBar({ userName, onLogin }) {
+  const getUserInitials = (name) => {
+    if (!name) return "";
+    const names = name.split(" ");
+    return names.map((n) => n[0].toUpperCase()).join("");
+  };
+
   return (
     <div className="nav-bar">
       <div className="left-content">
         {/* Any content you want on the left side */}
       </div>
-      <Link to="/">
-        <img src={logo} alt="Logo" className="logo" />
-      </Link>
-      <Link to="/listings/new" className="sell-button">
-        Sell
-      </Link>
+      <div className="navbar-logo">
+        <Link to="/">
+          <img src={logo} alt="Logo" className="logo" />
+        </Link>
+      </div>
+      <div className="navbar-actions">
+        <Link to="/listings/new" className="sell-button">
+          Sell
+        </Link>
+        {userName ? (
+          <div className="user-initials-circle">
+            {getUserInitials(userName)}
+          </div>
+        ) : (
+          <button className="login-button" onClick={onLogin}>
+            Log In
+          </button>
+        )}
+      </div>
     </div>
   );
-};
+}
 
 export default NavBar;
